86e39fe4479103601cb3eee8efcaa077ecd4da1e
Odette Bahome <odettebahome@gmail.com>
2020-11-24 23:46:10 +0000

    lotide project - tail

---
+++ tail.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+let tail = function(arr) {
+  return arr.slice(1);
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs");
