5059576b9883d80203472dc120348e9743fbb62d
Odette Bahome <odettebahome@gmail.com>
2020-11-24 23:44:03 +0000

    lotide project - assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// Function implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+// Test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
