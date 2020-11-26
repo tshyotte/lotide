2bd37889a98998f6987e9fe01717ff36d5661ac9
Odette Bahome <odettebahome@gmail.com>
2020-11-26 13:30:38 +0000

    eslint checked

--- countOnly.js
+++ countOnly.js
@@ -11,19 +11,18 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    console.log(item);
-    if(itemsToCount[item]) {
+  
+    if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
-      }
-      else {
+      } else {
         results[item] = 1;
       }
     }
   }
 
   return results;
-}
+};
 
 // Test code
 
