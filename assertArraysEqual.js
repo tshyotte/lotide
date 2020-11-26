fdf3b2fcda6b9f097fdfd5bbf5f89a7ec7ec1ba2
Odette Bahome <odettebahome@gmail.com>
2020-11-26 13:29:47 +0000

    eslint checked

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,4 @@
-// Function implementation
-/*const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-*/
+
 const eqArrays = function(arr1, arr2) {
 
   if (arr1.length !== arr2.length) {
