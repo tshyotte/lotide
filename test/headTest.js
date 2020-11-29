c26b8f72ee69b12dc5213523088fa37d3da3767a
Odette Bahome <odettebahome@gmail.com>
2020-11-29 23:34:36 +0000

    testing using mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,28 @@
+const assert = require('chai').assert;
 const head = require('../head');
 
 //test code
-head([5,6,7]);
-head(["Hello", "Lighthouse", "Labs"]);
-head([6]);
-head([]);
+//assertEqual(head([5,6,7]), 5);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"], "Hello");
+//head([6]);
+//head([]);
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("retuns 'Hello' for 'Hello'", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+  
+  it("retuns '' for empty array", () => {
+    assert.strictEqual(head([""]), "");
+  });
+
+});
 
