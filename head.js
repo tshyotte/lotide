8ac65a8613897c3410b6f2d1a4b4562571836dd0
Odette Bahome <odettebahome@gmail.com>
2020-11-29 21:29:12 +0000

    dry code head.js

--- head.js
+++ head.js
@@ -1,16 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
+const assertEqual = require('./assertEqual');
+
 function head(arr) {
   return arr[0];
 
-}
+};
+//test code
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([6]), 6);
 assertEqual(head([]), "Hi");
+
+module.exports = head;
