638d9d188497a28aae9c20214afb8711b93eeacf
Odette Bahome <odettebahome@gmail.com>
2020-11-30 01:21:11 +0000

    editted

--- head.js
+++ head.js
@@ -2,11 +2,6 @@ const assertEqual = require('./assertEqual');
 
 function head(arr) {
   return arr[0];
-
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([6]), 6);
-assertEqual(head([]), "Hi");
 
 module.exports = head;
