81658aa1730ae8d21899176c2e42c4662f4b29a7
Odette Bahome <odettebahome@gmail.com>
2020-11-30 01:22:33 +0000

    mocha test update

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,22 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
 // Test code
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
+
+describe("#assertEqual", () => {
+  it("returns false for 'Lighthouse Labs' and 'Bootcamp'", () => {
+    assert.strictEqual(assertEqual('Lighthouse Labs', 'Bootcamp'), false);
+  });
+
+  it("returns true for 'Hello' and 'Hello'", () => {
+    assert.strictEqual(assertEqual('Hello', 'Hello'), true); 
+  });
+
+  it("retuns true for 13 equal 13", () => {
+    assert.strictEqual(assertEqual(13, 13), true);
+  });
+
+});
+    
