45fb1ee717856b178de57f046981e291393632b4
Odette Bahome <odettebahome@gmail.com>
2020-11-30 01:25:14 +0000

    mocha test for eqArrays

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,28 @@
+//const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
+const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
 //test code
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 21, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 5]), false);
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+//assertEqual(eqArrays([1, 2, 3], [1, 21, 3]), true);
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 5]), false);
+
+describe("#eqArray", () => {
+  it("returns true for [1, 2, 3] and [1, 2, 3]", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it("returns false for [1, 2, 3] and [1, 21, 3]", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 21, 3]), false); 
+  });
+
+  it("retuns false for [1, 2, 3], [1, 2, 3, 5]", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3, 5]), false);
+  });
+  
+  //it("retuns '' for empty array", () => {
+   // assert.strictEqual(head([""]), "");
+  //});
+
+});
