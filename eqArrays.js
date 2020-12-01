a8a3b18ba0d580e157c1b6ac67ef856447753218
Odette Bahome <odettebahome@gmail.com>
2020-12-01 19:54:52 +0000

    edit

--- eqArrays.js
+++ eqArrays.js
@@ -2,12 +2,14 @@
 const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2) {
+  const len1 = arr1.length;
+  const len2 = arr2.length;
 
-  if (arr1.length !== arr2.length) {
+  if (len1 !== len2) {
     return false;
   }
   let i = 0;
-  while (i < arr1.length) {
+  while (i < len1) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
